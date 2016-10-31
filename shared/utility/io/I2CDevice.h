dd6fc327bb7d4995c338a096932e6dedeb63ffb5
Trent Houliston <trent@houliston.me>
2016-10-31 22:01:23 +1100

    Add endian swapping code

--- shared/utility/io/I2CDevice.h
+++ shared/utility/io/I2CDevice.h
@@ -29,12 +29,38 @@ namespace utility {
 
             template <uint8_t addr, typename T, enum Endian E>
             struct I2CVar {
+            private:
+                bool system_endian() {
+                    uint16_t number = 0x01;
+                    return *reinterpet_cast<char*>(&number) == 0x01 ? Endian::LE : Endian::BE;
+                }
+
+                T swap_endian(T data) {
+                    // Make an output variable
+                    T output;
+
+                    // Access our data bytewise
+                    char* in = reinterpet_cast<char*>(&data);
+                    char* out = reinterpet_cast<char*>(&out);
+
+                    // Swap the bytes
+                    for (uint i = 0; i < sizeof(T); ++i) {
+                        out[i] = in[sizeof(T) - i - 1];
+                    }
+
+                    // Return our output
+                    return output;
+                }
+
             public:
                 I2CVar(I2CDevice& device)
                 : device(device) {
                 }
 
-                operator= (const T&& data) {
+                operator= (T data) {
+                    // Swap our endian if needed
+                    T send = E == system_endian() : data ? swap_endian(data);
+
                     // Write the data
                     i2c_smbus_write_word_data(device.fd, sizeof(data), &data);
                 }
@@ -42,8 +68,10 @@ namespace utility {
                 operator T() const {
                     // Read the data
                     T data;
-                    i2c_smbus_write_word_data(device.fd, sizeof(data), &data);
-                    return data;
+                    i2c_smbus_write_read_data(device.fd, sizeof(data), &data);
+
+                    // Return, swapping the endianness if needed
+                    return E == system_endian() : data ? swap_endian(data);
                 }
 
             private:
